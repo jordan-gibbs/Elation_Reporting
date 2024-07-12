ca28a575fc7b34cba5fe2e08f73f4a95d6427bda
Jordan Gibbs <139834037+jordan-gibbs@users.noreply.github.com>
2024-07-12 09:15:47 -0600

    Update workplace_culture.py

--- workplace_culture.py
+++ workplace_culture.py
@@ -744,8 +744,8 @@ def subgroup_table(raw_df, org_name, subgroup, output_df, demo):
     elements.append(subtitle_table)
 
     header_table_data = [[Paragraph(header_data3[0], header_style), Paragraph(header_data3[1], header_style),
-                          Paragraph(header_data3[2], header_style), Paragraph(header_data3[3], header_style)]]
-    header_table = Table(header_table_data, colWidths=[235,105,105,105])
+                          Paragraph(header_data3[2], header_style)
+    header_table = Table(header_table_data, colWidths=[235,105,210])
     header_table.setStyle(TableStyle([
         ('BACKGROUND', (0, 0), (-1, -1), colors.white),
         ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
@@ -759,7 +759,7 @@ def subgroup_table(raw_df, org_name, subgroup, output_df, demo):
     elements.append(header_table)
 
     # Create and style the body table
-    table = Table(body_data_wrapped3, colWidths=[235,105,105,105])
+    table = Table(body_data_wrapped3, colWidths=[235,105,210])
     style = TableStyle([
         ('BACKGROUND', (0, 0), (-1, 0), colors.white),
         ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
@@ -802,8 +802,8 @@ def subgroup_table(raw_df, org_name, subgroup, output_df, demo):
     elements.append(subtitle_table)
 
     header_table_data = [[Paragraph(header_data3[0], header_style), Paragraph(header_data3[1], header_style),
-                          Paragraph(header_data3[2], header_style), Paragraph(header_data3[3], header_style)]]
-    header_table = Table(header_table_data, colWidths=[235,105,105,105])
+                          Paragraph(header_data3[2], header_style)]]
+    header_table = Table(header_table_data, colWidths=[235,105,210])
     header_table.setStyle(TableStyle([
         ('BACKGROUND', (0, 0), (-1, -1), colors.white),
         ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
@@ -817,7 +817,7 @@ def subgroup_table(raw_df, org_name, subgroup, output_df, demo):
     elements.append(header_table)
 
     # Create and style the body table
-    table = Table(body_data_wrapped4, colWidths=[235,105,105,105])
+    table = Table(body_data_wrapped4, colWidths=[235,105,210])
     style = TableStyle([
         ('BACKGROUND', (0, 0), (-1, 0), colors.white),
         ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
